0997a2b072430ebadb2e0154b1ed3492786af626
Ryan Scarff <williamscarff@Williams-MacBook-Pro.local>
2018-01-22 07:03:04 -0800

    Wins, and Losses working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,17 +26,49 @@
 //					display[i] = answer[i];
 //					console.log(display[i]);}}}}
 var possible = "abcdefghijklmnopqrstuvwxyz";
-var wins = 0
-var losses = 0
-var guessLeft = 15
+var wins = 0;
+var losses = 0;
+var guessLeft = 15;
+var display = "";
 
 document.getElementById("winCount").textContent = wins;
 document.getElementById("lossCount").textContent = losses;
 document.getElementById("left").textContent = guessLeft
 
 function startGame() {
-	var display = "";
 	var answer = possible.charAt(Math.floor(Math.random() * possible.length));
 	console.log(answer);
+	document.onkeyup = function(event) {
+		userInput = event.key;
+		if (userInput === answer) {
+			alert("You have guessed correctly!");
+			wins ++;
+			document.getElementById("winCount").textContent = wins;
+		} 
+		else if (alreadyCheck(userInput)) {
+		}
+		else {
+			guessCount(userInput)
+		}
+		if (guessLeft === 0) {
+			alert("You Lost");
+			losses ++;
+			document.getElementById("lossCount").textContent = losses;
+		}
+	}
+}
+
+
+
+function userWins () {
+	alert("You have guessed correctly!");
+	wins ++;
+}
+function guessCount (event) {
+	console.log(event);
+	guessLeft --;
+			display = display + event + ",";
+			document.getElementById("left").textContent = guessLeft;
+			document.getElementById("already").textContent = display;
 }
 startGame()
